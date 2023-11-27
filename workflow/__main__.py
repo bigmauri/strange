d9a5f9f1c04da093ef444908a834a53b6b4a6def
bigmauri <maurizio.bussi.mb@gmail.com>
2023-11-27 23:56:25 +0100

    feat(app): add argument parser to the python package
    Signed-off-by: bigmauri <maurizio.bussi.mb@gmail.com>

--- workflow/__main__.py
+++ workflow/__main__.py
@@ -1,18 +1,66 @@
+"""
+Entrypoint for the 'strange' python module.
+
+Description:
+    It is a WORKFLOW Engine.
+
+---
+
+Example
+
+SIMPLE USAGE
+1) Simple Case
+>>> python -u -m workflow --debug --run  # it runs the '.workflow.yaml' file in the current directory
+
+Thanks to 'git' most beautiful VCS.
+Thanks to 'python', it makes me feel good while coding as always.
+"""
+import argparse
 import json
 import logging
 import os
 
 from workflow.core.pipelines import Pipeline
 
-
+# LOG - Setup
 logging.basicConfig(level=logging.DEBUG)
 
 
-def main():
-    pipeline: Pipeline = Pipeline()
-    pipeline.run()
-    print(json.dumps(pipeline.info(), indent=2))
+# STATIC VARIABLES - Setup
+WORKFLOW_EXTENSION = ".workflow.yaml"
+
+def exit_in_case_no_workflow():
+    """
+    Terminate the application in case no '*.workflow.yaml' reached from the command line parser and
+    pretty print the parser.help in console.
+
+    :return: None   
+    """
+    if not any([f.endswith(WORKFLOW_EXTENSION) for f in os.listdir()]):
+        print()
+        parser.print_help()
+        print()
+        exit(0)
 
 
-if __name__ == '__main__':
-    main()
+# ARGUMENT PARSER - Setup
+parser = argparse.ArgumentParser(description="Workflow engine command line interface")
+parser.add_argument(
+    "--debug",
+    action="store_true",
+    help="Print out the JSON result of the pipeline workflow"
+)
+parser.add_argument(
+    "--run",
+    action="store_true",
+    help="Print out the JSON result of the pipeline workflow"
+)
+arguments = parser.parse_args()
+
+# CURRENT DIRECTORY - Validation
+exit_in_case_no_workflow()
+
+# WORKFLOW PIPELINE - Execution
+pipeline: Pipeline = Pipeline()
+pipeline.run() if arguments.run else None
+print(json.dumps(pipeline.info(), indent=2)) if arguments.debug else None
